9c83e234541b03a96d21eb351a8e5efae452610f
mrblup <83052922+mrblup@users.noreply.github.com>
2021-04-23 15:11:57 +1000

    Add files via upload

---
+++ balls.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class balls{
+    public static void main(String[]args){
+        Scanner sr = new Scanner(System.in);
+        String opop;
+        opop = sr.next();
+        System.out.println("hello world" + opop);
+        sr.close();
+    }
+}
